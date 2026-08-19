1a8658f2ca5b56ece15118982fb4a00920202666
Ossai001 <chukzeewilcox@yahoo.com>
2026-08-19 02:51:35 +0100

    Add expressions and alert JavaScript project

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+// Create the first string variable.
+var Sent1 = "This is the beginning of the string";
+
+// Create the second string variable.
+var Sent2 = " and this is the end of the string";
+
+// Concatenate the two variables.
+var completeSentence = Sent1 + Sent2;
+
+// Display the concatenated string in an alert box.
+window.alert(completeSentence);
+
+// Display the concatenated string on the webpage.
+document.write("<p>" + completeSentence + "</p>");
+
+// Create a mathematical expression.
+var result = 10 + 5;
+
+// Display the result of the expression.
+document.write("<p>The result of 10 + 5 is " + result + ".</p>");
